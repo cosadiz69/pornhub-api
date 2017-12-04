1a708e3c9b8b847e0ad54327eeb9570bc10f9d2d
cosadiz69 <cosadiz69@gmail.com>
2017-12-05 01:51:32 +0800

    first commit

---
+++ example.py
@@ -0,0 +1,1 @@
+import pornhubfrom multiprocessing import queueimport queuesearch_keywords = []client = pornhub.PornHub(search_keywords)for star in client.getStars(10):    print star    print star["mako oda"]    for video in client.getVideos(10,page=2):    print video    for photo_url in client.getPhotos(5):    print photo_url    print "All done!"
